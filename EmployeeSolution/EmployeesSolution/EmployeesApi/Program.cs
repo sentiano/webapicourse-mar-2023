2e76d3cefdd1e2bd54370169c09a786bc0f8036e
Tran Lambert <sentiano@yahoo.com>
2023-03-21 09:42:34 -0400

    update

---
+++ EmployeeSolution/EmployeesSolution/EmployeesApi/Program.cs
@@ -0,0 +1,37 @@
+
+using EmployeesApi.Domain;
+
+namespace EmployeesApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Startup ConfigureServices
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<DepartmentsLookup>();
+            var app = builder.Build();
+            
+            // Startup Configure
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
